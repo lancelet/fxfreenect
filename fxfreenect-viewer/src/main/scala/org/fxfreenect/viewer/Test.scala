4712e7e970ea1211374b2b930ea266736b6fe08b
Jonathan Merritt <j.s.merritt@gmail.com>
2013-12-16 21:46:52 +1100

    Initial RGB video stream support.
    Resolution switching is not working correctly: have to investigate.

--- fxfreenect-viewer/src/main/scala/org/fxfreenect/viewer/Test.scala
+++ fxfreenect-viewer/src/main/scala/org/fxfreenect/viewer/Test.scala
@@ -6,7 +6,10 @@ import scalafx.scene.Scene
 import scalafx.stage.Stage
 
 import org.fxfreenect.kinect._
-import org.openkinect.freenect.LedStatus;
+import org.openkinect.freenect.{Resolution, LedStatus}
+import scalafx.scene.image.ImageView
+import scalafx.scene.control.ComboBox
+import scalafx.event.ActionEvent
 
 object Test extends JFXApp {
 
@@ -27,11 +30,43 @@ object Test extends JFXApp {
   // set LED
   for (k <- kinect) k.setLedStatus(LedStatus.fromInt(LedStatus.BLINK_GREEN.intValue))
 
+  // set up RGB image view
+  val imageViewRGB = new ImageView
+  for (k <- kinect) {
+    imageViewRGB.image <== k.videoImageProperty
+    k.startVideo()
+    k.setVideoResolution(Resolution.LOW)
+  }
+
+  // resolution of RGB image
+  val comboBox = new ComboBox[String]
+  comboBox.getItems.add("LOW")
+  comboBox.getItems.add("MEDIUM")
+  comboBox.getItems.add("HIGH")
+  comboBox.onAction = {(e: ActionEvent) => {
+    val s: String = comboBox.getValue
+    val r: Resolution = s match {
+      case "LOW"    => Resolution.LOW
+      case "MEDIUM" => Resolution.MEDIUM
+      case "HIGH"   => Resolution.HIGH
+    }
+    // TODO: Investigate why mode-switching is not working.
+    for (k <- kinect) {
+      println(s"Setting resolution to ${comboBox.getValue}")
+      k.setVideoResolution(r)
+    }
+  }}
+
   stage = new JFXApp.PrimaryStage {
     title = "Test"
     width = 600
     height = 450
-    scene = new Scene { }
+    scene = new Scene {
+      content = Set(
+        imageViewRGB,
+        comboBox
+      )
+    }
   }
 
   override def stopApp() {
